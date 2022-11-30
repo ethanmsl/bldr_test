b17692ffd7330865327fe078c015a5c767302e00
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-30 06:23:20 -0800

    added tests, typing flag file, and re-wrote main for easier testing

--- src/bldr_test/main.py
+++ src/bldr_test/main.py
@@ -2,15 +2,34 @@
 
 
 import random
+from typing import TypeVar
 
 random.seed(12)
 
+
+def write_randoms(num_to_write: int) -> list[float]:
+    """write randoms"""
+    return [random.random() for _ in range(num_to_write)]
+
+
+T = TypeVar("T")
+# ugh, won't let me use type variable because not using for enough constraint
+# I kinda get it, since it's not going to create specialized sub functions
+# but also seems to complain about 'Any'
+# and I'm not sure how to provide a trait-like element
+# so that it only takes types thatre *`printable`* for example
+def print_look_at_array_val(array: list[int | float]) -> None:
+    """print look at array val"""
+    for val in array:
+        print(f"Look at this array value: {val}")
+
+
 def print_randoms(times_printed: int) -> int:
     """randoms"""
-    for _ in range(times_printed):
-        print(f"Look, here's a random number {random.random()}")
-
+    array = write_randoms(times_printed)
+    print_look_at_array_val(array)
     return 0
+
 
 def main() -> int:
     """main function"""
---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+from bldr_test import main
+
+def test_main():
+    """testing exit code 0; which actually tests for errors"""
+    assert main.main() == 0
+
+def test_print_randoms():
+    """testing exit code 0; which actually tests for errors"""
+    assert main.print_randoms(10) == 0
+
+def test_write_randoms():
+    "testing length of array returned"
+    assert len(main.write_randoms(10)) == 10
+
+def test_print_look_at_array_val():
+    "testing that there is no return (this is kinda silly perhaps)"
+    assert main.print_look_at_array_val([1, 2, 3]) is None
